b47d13e968a2100500bc5be17a3b07f4b5373148
Neuy <neuwirth.andrew9@gmail.com>
2023-01-04 23:34:04 -0500

    contact css added

--- portfolio/src/components/contact.js
+++ portfolio/src/components/contact.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import 'bulma/css/bulma.css';
+import React from "react";
+import "bulma/css/bulma.css";
 
 function Contact() {
+  return (
+    <div className="column is-3">
+      <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="e.g Alex Smith" />
+        </div>
+      </div>
 
-    return (
-        
-    )
+      <div class="field">
+        <label class="label">Email</label>
+        <div class="control">
+          <input
+            class="input"
+            type="email"
+            placeholder="e.g. alexsmith@gmail.com"
+          />
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Message</label>
+        <div class="control">
+        <textarea class="textarea" placeholder="e.g. Hello world"></textarea>
+        </div>
+      </div>
+      <button class="button">Submit</button>
+    </div>
+  );
 }
 
 export default Contact;
