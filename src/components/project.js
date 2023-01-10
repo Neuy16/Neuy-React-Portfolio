3e14de42785b00b5734859365d93c68c7e04445a
Neuy <neuwirth.andrew9@gmail.com>
2023-01-09 20:43:56 -0500

    added projects

--- src/components/project.js
+++ src/components/project.js
@@ -2,6 +2,11 @@ import React from "react";
 import "bulma/css/bulma.css";
 import meathead from '../assets/images/meathead.PNG';
 import utility from '../assets/images/cover.PNG';
+import myTrybe from '../assets/images/myTrybe.PNG';
+import textEditor from '../assets/images/textEditor.PNG';
+import oldPortfolio from '../assets/images/oldPortfolio.PNG';
+import teamProfileBuilder from '../assets/images/teambuild.PNG';
+
 
 function Project() {
   const projects = [
@@ -16,16 +21,40 @@ function Project() {
         url: "https://neuy16.github.io/Utility-Homepage-Project/",
         repoUrl: "https://github.com/Neuy16/Meathead",
         key: "meathead"
-    }
+    },
+    {
+        image: myTrybe,
+        url: "https://neuy16.github.io/Utility-Homepage-Project/",
+        repoUrl: "https://github.com/Neuy16/My-Trybe-Social",
+        key: "MyTrybe"
+    },
+    {
+        image: textEditor,
+        url: "https://neuy16.github.io/Utility-Homepage-Project/",
+        repoUrl: "https://github.com/Neuy16/PWA-Text-Editor",
+        key: "TextEditor"
+    },
+    {
+        image: oldPortfolio,
+        url: "https://neuy16.github.io/Portfolio/",
+        repoUrl: "https://github.com/Neuy16/Portfolio",
+        key: "OriginalPortfolio"
+    },
+    {
+        image: teamProfileBuilder,
+        url: "https://neuy16.github.io/Utility-Homepage-Project/",
+        repoUrl: "https://github.com/Neuy16/teamProfileBuilder",
+        key: "teamBuilder"
+    },
   ];
   return (
-    <div className="columns">
+    <div className="columns is-flex-direction-column">
         {projects.map((project) => (
-            <div className="column is-half mx-auto" key={project.key}>
+            <div className="column mx-auto" key={project.key}>
                 <a href={project.url}>
                     <img src={project.image}/>
                 </a>
-                <a href={project.repoUrl} key={project.key} className="columns is-size-3 is-centered has-text-success">
+                <a href={project.repoUrl} key={project.key} className="column is-size-3 is-centered has-text-success">
                 Link to the Github repository
                 </a>
             </div>
